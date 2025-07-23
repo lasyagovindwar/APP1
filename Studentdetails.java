9b7466928f2fb43894cfb5a075bb88cb87997a27
lasyagovindwar <165920927+lasyagovindwar@users.noreply.github.com>
2025-07-23 12:19:26 +0530

    Add files via upload

---
+++ Studentdetails.java
@@ -0,0 +1,11 @@
+public class Studentdetails {
+public static void main(String[] args) {
+String name = "Lasya";
+String rollNo = "245122748062";
+String department = "Computer Science(Ai&Ml)";
+System.out.println("Student Name: " + name);
+System.out.println("Roll No: " + rollNo);
+System.out.println("Department: " + department);
+}
+}
+
